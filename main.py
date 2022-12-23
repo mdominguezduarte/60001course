6a4f2892e79146cd4d17ffc300f52569b8ec843d
Mar <mdominguezduarte@gmail.com>
2022-12-23 16:28:49 +0100

    Fifth and Sixth lecture

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 # This is a sample Python script.
-import secondlecture
-import thirdlecture
-import fourthlecture
-
-
-print(fourthlecture.sqrt_int(5))
-
-
+import Sixthlecture
+test = "The_Princess_ofWales_has paid tribute to the late Queen Elizabeth in a special broadcast set to air on Christmas Eve. Catherine said the Queen had held Christmas close to her heart as a time that reminded us of the importance of faith, friendship and family. People recognised for showcasing community values were invited to the service at Westminster Abbey. It is the first royal carol service since the Queen died in September. The Together at Christmas event was held on 15 December, and was supported by the Royal Foundation, a charity founded and run by the Prince and Princess of Wales"
+result = Sixthlecture.frequence_words(test)
+print(result)
